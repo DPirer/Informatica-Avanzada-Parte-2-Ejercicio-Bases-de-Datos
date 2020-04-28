f1785f844e14cb9dd24396ca36fd01b25b5b7738
DPirer <46972090+DPirer@users.noreply.github.com>
2020-04-28 15:09:23 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -3,14 +3,25 @@
 #include "sqlite3.h"
 #include <string.h>
 
-// VARIABLES GLOBALES
+// VARIABLES GLOBALES *******************************************************************************************************/
 sqlite3 *db;
 int res;
 char *error = 0;
 char *nombre;
 int contador = 0;
 int numero_comando = 0;
+int contador2;
+int contadorfinal = 0;
 
+/* Consideramos que se piden ciertos datos porque son de importancia para la empresa, de forma que en vez de simplemente
+ * imprimirlos, los almacenaremos en una estructura, de forma que podamos operar con ellos, por ejemplo en el caso de
+ * las horas*/
+
+/* Para poder concatenar cadenas de caracteres, éstas tienen que tener una longitud. No podemos concaternar
+ * cadenas definidas por punteros*/
+
+/* En todo momento, el número máximo de personas trabajando en un mismo proyecto es de 2, luego creamos un array de
+ * 2 empleados, pero podríamos aumentar el número si se necesitaran más */
 
 struct Empleados{
     char nombre[100];
@@ -21,44 +32,70 @@ struct Empleados{
     char horas[100];
     char numero_departamento[100];
     char id_empleado[100];
-}empleado[10];
+}empleado[2];
 
 
-// PROTOTIPOS DE FUNCIONES
+// PROTOTIPOS DE FUNCIONES **************************************************************************************************/
+
 void abrirBase(char (*nombre_base)[]);
 void consultaProyecto(char (*nombre_proyecto)[]);
-static int imprimirDatos(void *nada, int longitud, char **Dato, char **NombreColumna);
+static int obtenerDatos(void *nada, int longitud, char **Dato, char **NombreColumna);
 
 
-// MAIN
+// MAIN  ********************************************************************************************************************/
+
 int main() {
 
     // Le pedimos al usuario que introduzca el nombre de la base de datos que quiere consultar y lo almacenamos
     // en una variable, que enviamos como puntero a la función abrir base para que la abra
     char nombre_base[20];
 
-    printf("\t\t.:MENU:.\n\n");
+    printf("\t.:MENU:.\n\n");
     printf("Introduzca el nombre de la base de datos a consultar: ");
     scanf("%s",&*nombre_base);
 
     // Abrimos la base de datos
     abrirBase(&nombre_base);
 
-    // Le pedimos al usuario el nombre del proyecto que quiere buscar
+    // Le pedimos al usuario el nombre del proyecto que quiere buscar. Modificamos scanf para que lea hasta el salto de línea
     char nombre_proyecto[20];
 
     printf("Introduzca el nombre del proyecto a consultar: ");
-    scanf("%s",&*nombre_proyecto);
+    scanf(" %[^\t\n]s",&*nombre_proyecto);
 
     // Y llamamos a la funcón que imprimirá los datos
     consultaProyecto(&nombre_proyecto);
 
+    /* Ahora ya tenemos una estructura con todos los datos más importantes de todos los empleados trabajando en un proyecto
+     * determinado. Creamos dos vectores donde almacenaremos el id de empleado, el numero de proyecto y las horas de cada empleado
+     * como enteros */
+    int l, num_proy, num_dpto, horas[2], ides[2];
+
+    num_proy = atoi(empleado[0].numero_proyecto);
+    num_dpto = atoi(empleado[0].numero_departamento);
+
+    for (l = 0; l < 2; l++){
+        ides[l] = atoi(empleado[l].id_empleado);
+        horas[l] = atoi(empleado[l].horas);
+    }
+
+    // Y finalmente, imprimirmos los datos en forma de tabla
+    printf("\n\nEMPLEADO             TRABAJA EN               ASOACIADO AL PROYECTO            DEPARTAMENTO           HORAS DE TRABAJO\n");
+    int i;
+    for (i = 0; i < contadorfinal; i++){
+        printf("%s %s           %s                      %s                          %s                  %s\n", empleado[i].nombre, empleado[i].apellido, empleado[0].ubicacion_departamento, empleado[0].numero_proyecto, empleado[0].nombre_departamento,empleado[i].horas);
+    }
+    printf("\n");
+
     // Cerramos la base de datos
     sqlite3_close(db);
+    
+    system("pause");
 }
 
 
-// FUNCIONES
+// FUNCIONES *****************************************************************************************************************/
+
 void abrirBase(char (*nombre_base)[]){
 
     res = sqlite3_open(*nombre_base, &db);
@@ -77,158 +114,174 @@ void abrirBase(char (*nombre_base)[]){
 
 void consultaProyecto(char (*nombre_proyecto)[]){
 
-    // Tengo que definir char de esta forma en vez de como puntero sin gamaño definido, porque si no, es una
-    // variable de sólo lectura, de forma que no podré concatenarla
+    // Se crea una variable que será una bandera, para indicarle a la función obtener dato dónde debe guardarse el mismo
+    // Después se crea el comando
     numero_comando = 1;
     char comando1[100] = "SELECT Numero_Dpto FROM Proyecto WHERE Proy_Nombre = '";
     strcat(comando1,*nombre_proyecto);
     strcat(comando1,"';");
 
-    // Ejecución de las instrucciones y llamamos a la función imprimir datos
-    res = sqlite3_exec(db, comando1, imprimirDatos, 0, &error);
+    // Y se llama a la función que obtendrá el dato diciéndole en qué empleado se guardará el dato,
+    // ya que contador es una variable global
+    contador = 0;
+    res = sqlite3_exec(db, comando1, obtenerDatos, 0, &error);
 
     numero_comando = 2;
     char comando2[100] = "SELECT Proy_Numero FROM Proyecto WHERE Proy_Nombre = '";
     strcat(comando2,*nombre_proyecto);
     strcat(comando2,"';");
 
-    printf("\n%s\n",comando2);
+    contador = 0;
+    res = sqlite3_exec(db, comando2, obtenerDatos, 0, &error);
 
-
-    // Ejecución de las instrucciones y llamamos a la función imprimir datos
-    res = sqlite3_exec(db, comando2, imprimirDatos, 0, &error);
-
+    // Conforme se obtienen datos, se usan para concanetar cadenas de caracteres que constituirán el siguiente comando
+    // para obtener la información necesaria
     numero_comando = 3;
     char comando3[100] = "SELECT Nombre_Dpto FROM Departamento WHERE Num_Dpto = ";
     strcat(comando3,empleado[0].numero_departamento);
     strcat(comando3,";");
 
-    printf("\n%s\n",comando3);
-
-    // Ejecución de las instrucciones y llamamos a la función imprimir datos
-    res = sqlite3_exec(db, comando3, imprimirDatos, 0, &error);
+    contador = 0;
+    res = sqlite3_exec(db, comando3, obtenerDatos, 0, &error);
 
     numero_comando = 4;
     char comando4[100] = "SELECT Ubicacion_Dpto FROM Ubicaciones_Departamento WHERE Numero_Dpto = ";
     strcat(comando4,empleado[0].numero_departamento);
     strcat(comando4,";");
 
-    printf("\n%s\n",comando4);
+    contador = 0;
+    res = sqlite3_exec(db, comando4, obtenerDatos, 0, &error);
 
-    // Ejecución de las instrucciones y llamamos a la función imprimir datos
-    res = sqlite3_exec(db, comando4, imprimirDatos, 0, &error);
-
+    /* El caso de obtener el ID de empleado es especial porque es el primer comando en el que tenemos dos salidas
+     * La función imprime línea a línea, y se vuelve a ejecutar tantas veces como filas haya. Aquí es cuando entra el contador
+     * que aumentará en uno su valor al terminar la ejecución de la función para que en la próxima ejecución, los datos
+     * se almacenen en el siguiente empleado */
     numero_comando = 5;
     char comando5[100] = "SELECT ID_Empleado FROM Trabaja_En WHERE Proy_Numero = ";
     strcat(comando5,empleado[0].numero_proyecto);
     strcat(comando5,";");
 
-    printf("\n%s\n",comando5);
+    contador = 0;
+    res = sqlite3_exec(db, comando5, obtenerDatos, 0, &error);
 
-    // Ejecución de las instrucciones y llamamos a la función imprimir datos
-    res = sqlite3_exec(db, comando5, imprimirDatos, 0, &error);
 
-    numero_comando = 6;
-    char comando6[100] = "SELECT Nombre_Pila FROM Empleado WHERE ID_Empleado =  ";
-    strcat(comando6,empleado[0].id_empleado);
-    strcat(comando6,";");
+    /* A partir de este momento, necesitamos el nombre, apellidos y horas de trabajo de empleados que tienen
+    dos ids distintos, luego tenemos que repetir la siguiente parte tantas veces como empleados haya trabajando
+    en un mismo proyecto, que en este caso es como máximo 2 */
+    int k;
+    for (k= 0; k<2; k++){
 
-    printf("\n%s\n",comando6);
+        // La variable global contador 2 nos asegurará que estamos guardando los datos en la función obtener datos
+        // en la posición correcta
+        contador2 = k;
 
-    // Ejecución de las instrucciones y llamamos a la función imprimir datos
-    res = sqlite3_exec(db, comando6, imprimirDatos, 0, &error);
+        // Y repetimos el mismo proceso que antes
+        numero_comando = 6;
+        char comando6[100] = "SELECT Nombre_Pila FROM Empleado WHERE ID_Empleado = ";
+        strcat(comando6,empleado[k].id_empleado);
+        strcat(comando6,";");
 
-    numero_comando = 7;
-    char comando7[100] = "SELECT Apellido_1 FROM Empleado WHERE ID_Empleado =  ";
-    strcat(comando7,empleado[0].id_empleado);
-    strcat(comando7,";");
+        contador = 0;
+        res = sqlite3_exec(db, comando6, obtenerDatos, 0, &error);
 
-    printf("\n%s\n",comando7);
+        numero_comando = 7;
+        char comando7[100] = "SELECT Apellido_1 FROM Empleado WHERE ID_Empleado = ";
+        strcat(comando7,empleado[k].id_empleado);
+        strcat(comando7,";");
 
-    // Ejecución de las instrucciones y llamamos a la función imprimir datos
-    res = sqlite3_exec(db, comando7, imprimirDatos, 0, &error);
+        contador = 0;
+        res = sqlite3_exec(db, comando7, obtenerDatos, 0, &error);
 
-    numero_comando = 8;
-    char comando8[100] = "SELECT Horas FROM Trabaja_En WHERE Proy_Numero =  ";
-    strcat(comando8,empleado[0].numero_proyecto);
-    strcat(comando8,";");
+        numero_comando = 8;
+        char comando8[100] = "SELECT Horas FROM Trabaja_En WHERE ID_Empleado = '";
+        strcat(comando8,empleado[k].id_empleado);
+        strcat(comando8,"' AND Proy_Numero = ");
+        strcat(comando8,empleado[0].numero_proyecto);
+        strcat(comando8,";");
 
-    printf("\n%s\n",comando8);
+        contador = 0;
+        res = sqlite3_exec(db, comando8, obtenerDatos, 0, &error);
 
-    // Ejecución de las instrucciones y llamamos a la función imprimir datos
-    res = sqlite3_exec(db, comando8, imprimirDatos, 0, &error);
-
-	int i;
-    // Una vez tenemos las estructuras rellenas, podemos imprimir los datos
-    printf("\n\nEMPLEADO             TRABAJA EN               ASOACIADO AL PROYECTO            DEPARTAMENTO           HORAS DE TRABAJO\n");
-    for ( i = 0; i < 2; i++){
-        printf("%s %s         %s                      %s                          %s                  %s\n", empleado[i].nombre, empleado[i].apellido, empleado[i].ubicacion_departamento, empleado[i].numero_proyecto, empleado[i].nombre_departamento,empleado[i].horas);
     }
-    printf("\n");
 
-    // Finalmente, cerramos la base de datos
-    sqlite3_close(db);
 }
 
-static int imprimirDatos(void *nada, int longitud, char **Dato, char **NombreColumna){
-    int i,j;
+static int obtenerDatos(void *nada, int longitud, char **Dato, char **NombreColumna){
 
-    for (i = 0; i<longitud; i++){
+		int j;
+        // A la hora de almacenar los datos, existirá solamente una columna y solamente un dato luego el dato será siempre el 0
 
+        /* Podríamos almacenar la cadena como un puntero, pero entonces la memoria es fija y no podemos realizar la concatenación.
+         * Necesitamos recorrer el vector de caracteres almacenándolos en la estructura de caracteres para luego poder concatenarlas. No se
+         * puede obtener tampoco el tamaño de un vector del que sólo conocemos el puntero, luego tenemos que poner un valor que sepamos
+         * que nunca vamos a alcanzar, en este caso escogemos 100*/
+
+        // Este primer switch contempla los primeros casos en los que el resultado es solamente un dato
         switch (numero_comando) {
         case 1:
-            for ( j = 0; j<100; j++){
-                empleado[i].numero_departamento[j] = Dato[i][j];
+            for (j = 0; j<100; j++){
+                empleado[0].numero_departamento[j] = Dato[0][j];
             }
-            printf("%s", empleado[i].numero_departamento);
             break;
         case 2:
-            for ( j = 0; j<100; j++){
-                empleado[i].numero_proyecto[j] = Dato[i][j];
+            for (j = 0; j<100; j++){
+                empleado[0].numero_proyecto[j] = Dato[0][j];
             }
-            printf("%s", empleado[i].numero_proyecto);
             break;
         case 3:
-            for ( j = 0; j<100; j++){
-                empleado[i].nombre_departamento[j] = Dato[i][j];
+            for (j = 0; j<100; j++){
+                empleado[0].nombre_departamento[j] = Dato[0][j];
             }
-            printf("%s", empleado[i].nombre_departamento);
             break;
         case 4:
-            for ( j = 0; j<100; j++){
-                empleado[i].ubicacion_departamento[j] = Dato[i][j];
+            for (j = 0; j<100; j++){
+                empleado[0].ubicacion_departamento[j] = Dato[0][j];
             }
-            printf("%s", empleado[i].ubicacion_departamento);
-            break;
-        case 5:
-            for ( j = 0; j<100; j++){
-                empleado[i].id_empleado[j] = Dato[i][j];
-            }
-            printf("%s", empleado[i].id_empleado);
-            break;
-        case 6:
-            for ( j = 0; j<100; j++){
-                empleado[i].nombre[j] = Dato[i][j];
-            }
-            printf("%s", empleado[i].nombre);
-            break;
-        case 7:
-            for ( j = 0; j<100; j++){
-                empleado[i].apellido[j] = Dato[i][j];
-            }
-            printf("%s", empleado[i].apellido);
-            break;
-        case 8:
-            for ( j = 0; j<100; j++){
-                empleado[i].horas[j] = Dato[i][j];
-            }
-            printf("%s", empleado[i].horas);
             break;
         }
-    }
 
-    printf("\n");
-    fflush(stdin);
+
+        /* Para el caso del ID, este switch almacenará los datos en la posición 0 en la primera iteración, luego aumentamos el
+         * contador a 1 para almacenarlo en la siguiente posición del array de estructuras */
+
+        switch (numero_comando) {
+        case 5:
+            for (j = 0; j<100; j++){
+                empleado[contador].id_empleado[j] = Dato[0][j];
+            }
+            contadorfinal++; // Este contador nos servirá para saber cuántos empleados hay a la hora de imprimirlos
+            break;
+        }
+
+
+        /* Y este switch es para el resto de valores posteriores al ID. En este caso, el contador 2 viene definiido por k,
+         *  que es la variable que nos indica con qué empleado estamos trabajando en este momento */
+        switch (numero_comando) {
+        case 6:
+            for (j = 0; j<100; j++){
+                empleado[contador2].nombre[j] = Dato[0][j];
+            }
+            break;
+        case 7:
+            for (j = 0; j<100; j++){
+                empleado[contador2].apellido[j] = Dato[0][j];
+            }
+            break;
+        case 8:
+            if (Dato[0] == NULL){ // En el caso de Reorganizacion, hay un campo en null que daría error
+                empleado[contador2].horas[0] = '0';
+            } else {
+                for (j = 0; j<100; j++){
+                    empleado[contador2].horas[j] = Dato[0][j];
+                }
+            }
+            break;
+        }
+
+        // Finalmente, aumentamos el contador para el caso del ID
+        contador++;
+        fflush(stdin);
 
     return 0;
 }
+
